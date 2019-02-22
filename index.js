0a79ebd09cabdd4529383669213d4cd3b3de4124
rchow1944 <rchow2@buffalo.edu>
2019-02-21 22:50:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+let formData = {
+  "firstName": "Raymond",
+  "registryMessage": "You have been added to the guest book"
 }
+
+let configData = {
+  method: "POST",
+  body: formData
+}
+
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", configData)
+  .then(res => res.json())
+  .then(content => content["message"])
+}
+
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", configData)
+  .then(res => res.json())
+  .then(content => console.log(content))
+  .catch(error => ":(")
+}
+registerSelf();
