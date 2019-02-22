524eddeecc5bd9ce3167f0189e71233c9c81af0d
Elizabeth Le <elizabethlemk@yahoo.com>
2019-02-22 11:43:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const testVar = {}
+function registerSelf() {
+  return fetch('http://guestbook.example.com/register',{
+    method: 'POST',
+    body: {
+      'firstName': "Lizzy",
+      'registryMessage': "Hello"
+    }
+  }).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      return json.message;
+    });
+}
 
-function testFunc() {
-  return "hi"
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", {
+    "method": "POST",
+    "body": {
+      "firstName": "Lizzy",
+      "registryMessage": "Hello"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  }).catch(function(x) {
+    return ":(";
+  });
 }
