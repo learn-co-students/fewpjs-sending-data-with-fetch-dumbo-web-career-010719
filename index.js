95aba391706b757d459da209215a354d0751bdee
woodylucas <wlucas0311@gmail.com>
2019-02-22 09:36:13 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const registerSelf = () => {
+  return fetch('http://guestbook.example.com/register', {
+    method:'POST',
+    body: {
+      firstName: 'Cuffy',
+      registryMessage: 'Combs'
+    }
+  })
+  .then(resp => resp.json())
+  .then(data => data.message)
+}
+
+const errorSelf = () => {
+  return fetch('http://guestbook.example.com/register-error', {
+    method: 'POST',
+    body: {
+      firstName: 'Sean',
+      registryMessage: 'Combs'
+    }
+  })
+  .then(resp => resp.json())
+  .then(data => data.message)
+  .catch(errors => ':(')
+}
